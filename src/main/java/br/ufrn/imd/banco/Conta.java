2d19814dd8cd1a4606484fa082829cd6b540d23c
Emanuel Kywal <emanuelkywal@outlook.com>
2025-04-22 21:00:33 -0300

    feat: empty and parameterized constructor on Conta class

--- src/main/java/br/ufrn/imd/banco/Conta.java
+++ src/main/java/br/ufrn/imd/banco/Conta.java
@@ -5,13 +5,6 @@ public class Conta {
     private String codigo;
     private double saldo = 0;
 
-    public Conta() {}
-
-    public Conta(String p_id, double p_saldo) {
-        this.saldo = p_saldo;
-        this.codigo = p_id;
-    }
-
     public double debitar (double valor) throws OperacaoIlegalException {
         if(valor > 0) {
             saldo = saldo-valor;
@@ -44,4 +37,26 @@ public class Conta {
         this.debitar(valor);
     }
 
+    public Conta() {}
+
+    public Conta(String p_id, double p_saldo) {
+        this.saldo = p_saldo;
+        this.codigo = p_id;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 }
