e1b7f4f437420b5277bc4b317bc0efcc71abe0a8
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-05 13:56:23 -0300

    feat(worksheet 2, question 12): Average class

---
+++ src/main/java/br/ufrn/imd/listas/exercicios02/questao12/Average.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.listas.exercicios02.questao12;
+
+public class Average {
+
+    private Average() {}
+
+    public static double semester(int num1, int num2, int num3) {return (num1 + num2 + num3) / 3;
+    }
+}
