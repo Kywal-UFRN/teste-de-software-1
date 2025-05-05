69cc1f15ca9b178615d01db391bcc930cc16f169
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-05 09:11:52 -0300

    feat(worksheet 2, question 3): FilmeNaoValidoException

---
+++ src/main/java/br/ufrn/imd/listas/exercicios02/questao03/FilmeNaoValidoException.java
@@ -0,0 +1,7 @@
+package br.ufrn.imd.listas.exercicios02.questao03;
+
+public class FilmeNaoValidoException extends Exception {
+    public FilmeNaoValidoException(String message) {
+        super(message);
+    }
+}
